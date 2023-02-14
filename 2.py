13545bd6ad6e7dd521a36be52ec1766243d45e6a
Рената <114076600+roraidega@users.noreply.github.com>
2023-02-14 11:48:43 +0300

    Delete 2.py

--- 2.py
+++
@@ -1,8 +0,0 @@
-"""
-Функция sorted принимает в качестве дополнительного параметра key(Можете почитать документацию).
-С помощью lambda-функции отсортируйте этот список словарей по именам
-"""
-
-
-grades = [{'name': 'Renata', 'final': 95}, {'name': 'Lera', 'final': 92}, {'name': 'Ann', 'final': 98}]
-print(sorted(grades, key=lambda x: x['name']))
