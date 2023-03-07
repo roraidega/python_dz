9a234d271a8509765b05deae3a343563ba8b64fc
Рената <114076600+roraidega@users.noreply.github.com>
2023-03-07 16:25:54 +0300

    ModuleOS

---
+++ moduleOS/main_4.py
@@ -0,0 +1,9 @@
+""" напишите программу которая создает папку task4 и записывает текст "я выполнил задание" в файл answer.txt
+"""
+import os
+
+
+os.mkdir(r"C:\Users\Рената\OneDrive\Рабочий стол\Бабчёнок Олег\task_4")
+os.chdir(r"C:\Users\Рената\OneDrive\Рабочий стол\Бабчёнок Олег\task_4")
+text_file = open('answer.txt', 'w')
+text_file.write('Я выполнила задания')
