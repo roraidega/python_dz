d039dac0d58db57fbe261b040a0cec188cb0ffcf
Рената <114076600+roraidega@users.noreply.github.com>
2023-03-07 16:57:37 +0300

    Delete main_4.py

--- main_4.py
+++
@@ -1,8 +0,0 @@
-""" напишите программу которая создает папку task4 и записывает текст "я выполнил задание" в файл answer.txt
-"""
-import os
-
-os.mkdir(r"C:\Users\Рената\OneDrive\Рабочий стол\Бабчёнок Олег\task_4")
-os.chdir(r"C:\Users\Рената\OneDrive\Рабочий стол\Бабчёнок Олег\task_4")
-text_file = open('answer.txt', 'w')
-text_file.write('Я выполнила задания')
