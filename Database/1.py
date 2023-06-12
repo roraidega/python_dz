3b3bbe000551a56bc48ef6cf8f03f5abc0437359
Рената <114076600+roraidega@users.noreply.github.com>
2023-06-12 13:50:40 +0300

    Database_relationship

--- Database/1.py
+++ Database/1.py
@@ -1,51 +1,31 @@
-"""
-Создайте базу данных фильмов состоящая из столбцов: id,название, год выпуска, жанр, рейтинг.
-Создайте функции для добавления фильма в базу, получения всех фильмов, получение фильма по определенному году, обновления рейтинга, удаления фильма.
-"""
-
-from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Session
-
-datab = "sqlite:///db_films.sql"
-engine = create_engine(datab)
-
+from sqlalchemy import *
+from sqlalchemy.orm import *
+sqlite_database = "sqlite:///person1.db"
+engine = create_engine(sqlite_database)
 
 class Base(DeclarativeBase): pass
-#базовый класс для моделей
-
-class Films(Base):
-    __tablename__ = 'Films'
+class User_book(Base):
+    __tablename__ = "User_book"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
-    date = Column(String)
-    genre = Column(String)
-    rating = Column(Integer)
+    book = relationship("Book", back_populates="users")
+
+
+class Book(Base):
+    __tablename__ = "book"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    author = Column(String)
+    id_user = Column(Integer, ForeignKey("User_book.id"))
+    users = relationship("User_book", back_populates="book")
 
 Base.metadata.create_all(bind=engine)
 
-def create(fi_name, fi_date, fi_genre, fi_rating):
-    with Session(autoflush=False, bind=engine) as bd:
-        f = Films(name = fi_name, date = fi_date, genre = fi_genre, rating = fi_rating)
-        bd.add(f)
-        bd.commit()
-
-create('КотикИ', '1985', 'Драма', '74')
-create('Крокодильчики', '8754', 'Мелодрама', '10000')
-create('Зайчики', '6740', 'Комедия', '41')
-create('Курочки', '9378', 'Фантастика', '89')
-
-def get_info():
-    with Session(autoflush=False, bind=engine) as bd:
-        f = bd.query(Films).all()
-        for i in f:
-            print(i.name)
-
-def delete(id_del):
-     with Session(autoflush=False, bind=engine) as bd:
-        kill = bd.query(Films).filter(Films.id==id_del).first()
-        bd.delete(kill)
-        bd.commit()
-
-delete(3)
+with Session(autoflush=False,bind = engine) as db:
+    def User_search_book():
+        input_user=input()
+        users = db.query(User_book).filter(User_book.name==input_user).all()
+        for user in users:
+            for book in user.book:
+                print(book.name)
+    User_search_book()
---
+++ Database/2.py
@@ -0,0 +1,66 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import DeclarativeBase, Session, relationship
+
+sql_database = "sqlite:///User_prof.db"
+engine = create_engine(sql_database)
+
+
+class Base(DeclarativeBase):
+    pass
+
+asocciation_table = Table('asocciation_table', Base.metadata,
+                          Column('user_post_id', Integer, ForeignKey('user_post.id')),
+                          Column('user_id', Integer, ForeignKey('user.id')))
+
+class User_post(Base):
+    __tablename__ = "user_post"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    list_user = relationship("User",secondary=asocciation_table, back_populates="user_post")
+
+
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    experience = Column(Integer)
+    user_post_id = Column(Integer, ForeignKey("user_post.id"))
+    user_post = relationship("User_post", secondary=asocciation_table, back_populates="list_user")
+
+
+
+Base.metadata.create_all(bind=engine)
+db = Session(autoflush=False, bind=engine)
+
+
+with Session(autoflush=False, bind=engine) as db:
+    user_post_1 = User_post(name="Дизайнер")
+    user_post_2 = User_post(name="Программист")
+    user_post_3 = User_post(name="Разработчик")
+
+    db.add_all([user_post_1, user_post_2, user_post_3])
+    db.commit()
+
+    people_1 = User(name="Yana", experience = 20)
+    people_2 = User(name="Ignat", experience = 21)
+    people_3 = User(name="Nikita", experience = 22)
+    people_1.user_post.append(user_post_1)
+    db.add_all([people_1, people_2, people_3])
+    db.commit()
+
+
+    user_input = input()
+    users_db = db.query(User).filter(User.name == user_input).all()
+    for i in users_db:
+        for j in i.user_post:
+            print(j.name)
+
+    user_input = input()
+    users_db = db.query(User).filter(User.experience >= 5).all()
+    for i in users_db:
+        for j in i.user_post:
+            if j.name == user_input:
+                print(i.name)
+
